08b5a3f1c81b31717dd9ecd584116c1a5b3fa2e1
Geovane Martins <geeow.2013@gmail.com>
2022-10-08 22:04:25 -0300

    Aplicando splash screen com logo da Unip

--- lib/home.dart
+++ lib/home.dart
@@ -7,6 +7,8 @@ import 'camera.dart';
 import 'bndbox.dart';
 import 'models.dart';
 
+import 'package:splashscreen/splashscreen.dart';
+
 class HomePage extends StatefulWidget {
   final List<CameraDescription> cameras;
 
@@ -172,4 +174,49 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
+}
+
+List<CameraDescription> cameras;
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return _introScreen();
+  }
+}
+
+Widget _introScreen() {
+  return Stack(
+    children: <Widget>[
+      SplashScreen(
+        seconds: 5,
+        gradientBackground: LinearGradient(
+          begin: Alignment.topRight,
+          end: Alignment.bottomLeft,
+          colors: [
+            Color.fromRGBO(30, 144, 255, 0),
+            Color.fromRGBO(30, 144, 255, 0)
+          ],
+        ),
+        navigateAfterSeconds: HomePage(cameras),
+        loaderColor: Colors.transparent,
+      ),
+      Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/logo.png"),
+            fit: BoxFit.none,
+          ),
+        ),
+      ),
+    ],
+  );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         brightness: Brightness.dark,
       ),
-      home: HomePage(cameras),
+      home: MyHomePage(title: 'Teste'),
     );
   }
 }
